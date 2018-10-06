a64a08abc1b44a6590f674c808fbb8d17697dd7e
Naten Baptista <naten.baptista@enepath.com>
2018-10-06 16:16:29 +0000

    Year added using js in copyright footer line.

--- script/home_page_script.js
+++ script/home_page_script.js
@@ -1,2 +1,5 @@
 function onLoadBody(){
+    var mDate = new Date();
+    var mYear = mDate.getFullYear();
+    document.getElementById('copyright_text').innerHTML = '&#169; Naten Baptista '+mYear+' All rights reserved.';
 }
